c6cb0f3f95292c3e8655117d1a5162979a55e5ee
cartedigital <cartshk94@gmail.com>
2026-09-04 19:58:19 -0600

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,5 +0,0 @@
-// Configuración de Supabase para CARTE AI
-const SUPABASE_URL = 'https://wehybszkkwvhrcsijkgl.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_hu-Ogpo6IRazjhoaOfwB4Q_MzSCbCg_';
-
-window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
